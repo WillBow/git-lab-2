70e22332092ec1631b26923e74d5eadfd3b4824e
Will Bower <wb726517@ohio.edu>
2019-01-22 19:15:40 -0500

    added git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Will Bower
+ *        Date: 1/22/19
+ * Description: program for CS 2400 Lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
